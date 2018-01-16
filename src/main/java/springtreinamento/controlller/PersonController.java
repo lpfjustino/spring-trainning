8519e0160306646c2132af6caa09de631976b8c5
Lpfjustino <lpfjustino@gmail.com>
2018-01-16 10:56:34 -0200

    Included exception handling

--- src/main/java/springtreinamento/controlller/PersonController.java
+++ src/main/java/springtreinamento/controlller/PersonController.java
@@ -1,6 +1,7 @@
 package springtreinamento.controlller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
@@ -22,41 +23,67 @@ public class PersonController {
 
   @Transactional
   @PostMapping
-  public @ResponseBody String addNewPerson (@Valid @RequestBody Person person) {
-    manager.persist(person);
-    return "Saved";
+  public @ResponseBody ResponseEntity<?> addNewPerson (@Valid @RequestBody Person person) {
+    try {
+      manager.persist(person);
+      return new ResponseEntity<>(HttpStatus.OK);
+    } catch (IllegalArgumentException e) {
+      return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+    }
   }
 
   @Transactional
   @PutMapping
-  public @ResponseBody String updatePerson (@Valid @RequestBody Person person) {
-    manager.merge(person);
-    return "Updated";
+  public @ResponseBody ResponseEntity<?> updatePerson (@Valid @RequestBody Person person) {
+    try {
+      Person updated = manager.merge(person);
+      return new ResponseEntity<>(updated, HttpStatus.OK);
+    } catch (IllegalArgumentException e) {
+      return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+    }
   }
 
   @GetMapping("/")
-  public @ResponseBody List<Person> lista() {
-    return manager.createQuery("select p from Person p").getResultList();
+  public @ResponseBody ResponseEntity<?> lista() {
+    try {
+      List<Person> list = manager.createQuery("select p from Person p").getResultList();
+      return new ResponseEntity<>(list, HttpStatus.OK);
+    } catch (IllegalArgumentException e) {
+      return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+    }
   }
 
   @GetMapping("/{id}")
-  public Person getPersonById(@PathVariable(value="id") Long id) {
-    return manager.find(Person.class, id);
+  public ResponseEntity<?> getPersonById(@PathVariable(value="id") Long id) {
+    try {
+      Person person = manager.find(Person.class, id);
+      return new ResponseEntity<>(person, HttpStatus.OK);
+    } catch (IllegalArgumentException e) {
+      return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+    }
   }
 
   @Transactional
   @DeleteMapping
-  public @ResponseBody String remove(@Valid @RequestBody Person person) {
-    Person removedPerson = getPersonById(person.getId());
-    manager.remove(removedPerson);
-    return "Removed";
+  public @ResponseBody ResponseEntity<?> remove(@Valid @RequestBody Person person) {
+    try {
+      Person removedPerson = manager.find(Person.class, person.getId());
+      manager.remove(removedPerson);
+      return new ResponseEntity<>(HttpStatus.OK);
+    } catch (IllegalArgumentException e) {
+      return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+    }
   }
 
   @Transactional
   @DeleteMapping("/{id}")
-  public @ResponseBody String remove(@PathVariable(value="id") Long id) {
-    Person removedPerson = getPersonById(id);
-    manager.remove(removedPerson);
-    return "Removed";
+  public @ResponseBody ResponseEntity<?> remove(@PathVariable(value="id") Long id) {
+    try {
+      Person removedPerson = manager.find(Person.class, id);
+      manager.remove(removedPerson);
+      return new ResponseEntity<>(HttpStatus.OK);
+    } catch (IllegalArgumentException e) {
+      return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+    }
   }
 }
